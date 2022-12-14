df6c48668c51809d3e3a8245ed9ef0dd6e1f364b
Antony Choi <antony.choi@oocl.com>
2022-12-14 14:57:32 +0800

    feat: add api get

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://63996b7a16b0fdad773c200d.mockapi.io/api/"
+    baseURL: "https://63996b7a16b0fdad773c200d.mockapi.io"
 });
 
 export const getTodos = () => {
--- src/features/todo/TodoList.js
+++ src/features/todo/TodoList.js
@@ -1,13 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
+import { useEffect } from "react"
+import { getTodos } from "../../api/todos";
+import { addTodos } from "./todoSlice";
 
 const TodoList = () => {
   // get the data from store
   const todos = useSelector((state) => {
     return state.todoList;
   });
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    getTodos().then((response) => {
+      dispatch(addTodos(response.data));
+    })
+  }, [dispatch])
   return (
     <>
       <TodoGroup todos={todos} />
