48acd96300f694a77b2967116582bfffda1f6e80
leungpo <polly.leung@oocl.com>
2022-12-15 20:49:51 +0800

    feat: change the URL to 8080 port for mongo connection

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://63996b3416b0fdad773c1841.mockapi.io/"
+    baseURL:" http://localhost:8080/"
 });
 
 export const getTodos = () => {
